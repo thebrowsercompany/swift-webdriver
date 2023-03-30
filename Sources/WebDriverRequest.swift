b249a65cd897aa254d8e5f4d091a260c691a819d
Tristan Labelle <tristan@thebrowser.company>
2023-03-30 10:06:21 -0400

    Remove commented out code

--- Sources/WebDriverRequest.swift
+++ Sources/WebDriverRequest.swift
@@ -7,19 +7,6 @@ struct WebDriverResponse<Value> : Decodable where Value : Decodable {
     var sessionId: String
     var status: Int?
     var value: Value
-
-    // init(from decoder: Decoder) throws {
-    //     let values = try decoder.container(keyedBy: CodingKeys.self)
-    //     sessionId = try values.decode(String.self, forKey: .sessionId)
-    //     status = try? values.decode(Int.self, forKey: .status)
-    //     value = try? values.decode(Value.self, forKey: .value)
-    // }
-
-    // enum CodingKeys: String, CodingKey {
-    //     case sessionId
-    //     case status
-    //     case value
-    // }
 }
 
 struct WebDriverNoResponseValue : Decodable {
