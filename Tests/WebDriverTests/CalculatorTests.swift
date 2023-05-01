40a4b76b63252d6aaf438a4a5fce54a3721419af
christianfo <15091389+christianfo@users.noreply.github.com>
2023-05-01 11:35:23 -0700

    Removed unused var and fixed curly brace style

--- Tests/WebDriverTests/CalculatorTests.swift
+++ Tests/WebDriverTests/CalculatorTests.swift
@@ -14,22 +14,15 @@ class CalculatorTests : XCTestCase {
         // We don't store webDriver as session maintains it alive
         let webDriver = WebDriver(url: winAppDriver.url)
         session = webDriver.newSession(app: "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App")
-    }
 
-    var header: Element?
-    var calculatorResult: Element?
-
-    // Called before each test
-    public override func setUp() {
-        header = Self.session.findElement(byAccessibilityId: "Header") ??
-                 Self.session.findElement(byAccessibilityId: "ContentPresenter")
-
+        // Locate the element with the result of the calculation
         calculatorResult = Self.session.findElement(byAccessibilityId: "CalculatorResults")!
     }
 
+    private static var calculatorResult: Element?
+
     // Called once after all tests in this class have run
     public override class func tearDown() {
-        // Force the destruction of the session
         session = nil
         winAppDriver = nil
     }
@@ -37,10 +30,10 @@ class CalculatorTests : XCTestCase {
     // Test methods
 
     private var calculatorResultText: String {
-        calculatorResult!.text.replacingOccurrences(of: "Display is", with: String()).trimmingCharacters(in: .whitespacesAndNewlines);
+        Self.calculatorResult!.text.replacingOccurrences(of: "Display is", with: String()).trimmingCharacters(in: .whitespacesAndNewlines);
     }
 
-   public func testAddition() {
+    public func testAddition() {
         // Find the buttons by their names and click them in sequence to perform 1 + 7 = 8
         Self.session.findElement(byName: "One")?.click();
         Self.session.findElement(byName: "Plus")?.click();
@@ -49,8 +42,7 @@ class CalculatorTests : XCTestCase {
         XCTAssertEqual("8", calculatorResultText);
     }
 
-    public func testDivision()
-    {
+    public func testDivision() {
         // Find the buttons by their accessibility ids and click them in sequence to perform 88 / 11 = 8
         Self.session.findElement(byAccessibilityId: "num8Button")?.click();
         Self.session.findElement(byAccessibilityId: "num8Button")?.click();
@@ -61,8 +53,7 @@ class CalculatorTests : XCTestCase {
         XCTAssertEqual("8", calculatorResultText);
     }
 
-    public func testMultiplication()
-    {
+    public func testMultiplication() {
         // Find the buttons by their names using XPath and click them in sequence to perform 9 x 9 = 81
         Self.session.findElement(byXPath:  "//Button[@Name='Nine']")?.click();
         Self.session.findElement(byXPath:  "//Button[@Name='Multiply by']")?.click();
@@ -71,8 +62,7 @@ class CalculatorTests : XCTestCase {
         XCTAssertEqual("81", calculatorResultText);
     }
 
-    public func testSubtraction()
-    {
+    public func testSubtraction() {
         // Find the buttons by their accessibility ids using XPath and click them in sequence to perform 9 - 1 = 8
         Self.session.findElement(byXPath:  "//Button[@AutomationId=\"num9Button\"]")?.click();
         Self.session.findElement(byXPath:  "//Button[@AutomationId=\"minusButton\"]")?.click();
