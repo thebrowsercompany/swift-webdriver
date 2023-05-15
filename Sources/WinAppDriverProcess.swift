caad23d703dda16a6ca4d2a2ece5df5b9e4f062c
Renato Mangini <mangini@gmail.com>
2023-05-15 11:41:07 -0600

    Add verbose to identify reason for tests timing out

--- Sources/WinAppDriverProcess.swift
+++ Sources/WinAppDriverProcess.swift
@@ -5,27 +5,28 @@ class WinAppDriverProcess {
     static let port = 4723
 
     let process: Process
-    let toStdinPipe: Pipe
+ //   let toStdinPipe: Pipe
 
     init() throws {
         let path = "\(ProcessInfo.processInfo.environment["ProgramFiles(x86)"]!)\\Windows Application Driver\\WinAppDriver.exe"
         
-        toStdinPipe = Pipe()
+//        toStdinPipe = Pipe()
         process = Process()
         process.executableURL = URL(fileURLWithPath: path)
         process.arguments = [ Self.ip, String(Self.port) ]
-        process.standardInput = toStdinPipe.fileHandleForReading
+//        process.standardInput = toStdinPipe.fileHandleForReading
         process.standardOutput = nil
         do {
             try process.run()
         } catch {
             fatalError("Could not start AppWinDriver!")
         }
+       // abort()
     }
 
     deinit {
         // WinAppDriver responds waits for a key to return
-        try? toStdinPipe.fileHandleForWriting.write(contentsOf: "\n".data(using: .utf8)!)
+//        try? toStdinPipe.fileHandleForWriting.write(contentsOf: "\n".data(using: .utf8)!)
         process.terminate()
     }
 
