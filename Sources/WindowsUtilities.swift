fa42939a3b3c6299e03b449eb5eefaa7d47090f1
christianfo <15091389+christianfo@users.noreply.github.com>
2023-05-28 07:56:00 -0700

    Better implementation of isProcessRunning(:)

--- Sources/WindowsUtilities.swift
+++ Sources/WindowsUtilities.swift
@@ -2,35 +2,38 @@ import Foundation
 import WinSDK
 
 // Helper to determine if a process with a given name is running
-func isProcessRunning(withName name: String) -> Bool {
-    let snapshotHandle = CreateToolhelp32Snapshot(DWORD(TH32CS_SNAPPROCESS), 0)
-    if snapshotHandle == INVALID_HANDLE_VALUE {
-        return false
-    }
-
-    defer {
-        CloseHandle(snapshotHandle)
-    }
-
-    var processEntry = PROCESSENTRY32W()
-    processEntry.dwSize = DWORD(MemoryLayout<PROCESSENTRY32W>.size)
-
-    if !Process32FirstW(snapshotHandle, &processEntry) {
-        return false
-    }
+func isProcessRunning(withName processName: String) -> Bool {
+    var processIds: [DWORD] = []
+    var bytesReturned: DWORD = 0
 
     repeat {
-        let capacity = MemoryLayout.size(ofValue: processEntry.szExeFile)
-        let processName = withUnsafePointer(to: &processEntry.szExeFile) {
-            $0.withMemoryRebound(to: WCHAR.self, capacity: capacity) {
-                String(decodingCString: $0, as: UTF16.self)
+        processIds = Array(repeating: 0, count: processIds.count + 1024)
+        if !K32EnumProcesses(&processIds, DWORD(processIds.count * MemoryLayout<DWORD>.size), &bytesReturned) {
+            return false
+        }
+    } while bytesReturned == DWORD(processIds.count * MemoryLayout<DWORD>.size)
+
+    let processCount = Int(bytesReturned) / MemoryLayout<DWORD>.size
+
+    for i in 0..<processCount {
+        let processId = processIds[i]
+        let processHandle = OpenProcess(DWORD(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ), false, processId)
+
+        if processHandle != nil {
+            defer {
+                CloseHandle(processHandle)
+            }
+
+            var processNameBuffer: [WCHAR] = Array(repeating: 0, count: Int(MAX_PATH))
+            if K32GetModuleBaseNameW(processHandle, nil, &processNameBuffer, DWORD(processNameBuffer.count)) > 0 {
+                let processNameString = String(decodingCString: processNameBuffer, as: UTF16.self)
+
+                if processNameString.lowercased() == processName.lowercased() {
+                    return true
+                }
             }
         }
-        if processName == name { 
-            return true
-        }
-    } while Process32NextW(snapshotHandle, &processEntry)
-
+    }
     return false
 }
 
