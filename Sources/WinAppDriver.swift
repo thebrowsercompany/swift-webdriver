78b28204f7e0168f4428291899fdf1b55016f5b9
christianfo <15091389+christianfo@users.noreply.github.com>
2023-05-30 15:06:46 -0700

    Don't start WinAppDriver if process already running (#22)

--- Sources/WinAppDriver.swift
+++ Sources/WinAppDriver.swift
@@ -1,4 +1,5 @@
 import Foundation
+import WinSDK
 
 public class WinAppDriver: WebDriver {
     static let ip = "127.0.0.1"
@@ -6,34 +7,43 @@ public class WinAppDriver: WebDriver {
 
     let httpWebDriver: HTTPWebDriver
 
-    public init() throws {
+    struct RunningProcess {
+        var process: Process
+        var toStdinPipe: Pipe
+    }
+    var runningProcess: RunningProcess? = nil
+
+    init() throws {
         httpWebDriver = HTTPWebDriver(endpoint: URL(string: "http://\(Self.ip):\(Self.port)")!)
+        
+        // We start WinAppDriver only if its process is not already started
+        // CI machines start it using a GitHub action before running the tests 
+        // to get around https://linear.app/the-browser-company/issue/WIN-569/winappdriver-does-not-work-on-ci
+        if !isProcessRunning(withName: "WinAppDriver.exe") {
+            let path = "\(ProcessInfo.processInfo.environment["ProgramFiles(x86)"]!)\\Windows Application Driver\\WinAppDriver.exe"
+
+            let process = Process()
+            let pipe = Pipe()
+            process.executableURL = URL(fileURLWithPath: path)
+            process.arguments = [ Self.ip, String(Self.port) ]
+            process.standardInput = pipe.fileHandleForReading
+            process.standardOutput = nil
+            runningProcess = RunningProcess(process: process, toStdinPipe: pipe)
+            do {
+                try runningProcess!.process.run()
+            } catch {
+                fatalError("Could not start WinAppDriver!")
+            }
+        }
     }
 
-    // let process: Process
-    // let toStdinPipe: Pipe
-
-    // init() throws {
-        // let path = "\(ProcessInfo.processInfo.environment["ProgramFiles(x86)"]!)\\Windows Application Driver\\WinAppDriver.exe"
-        
-        // toStdinPipe = Pipe()
-        // process = Process()
-        // process.executableURL = URL(fileURLWithPath: path)
-        // process.arguments = [ Self.ip, String(Self.port) ]
-        // process.standardInput = toStdinPipe.fileHandleForReading
-        // process.standardOutput = nil
-        // do {
-        //     try process.run()
-        // } catch {
-        //     fatalError("Could not start AppWinDriver!")
-        // }
-    // }
-
-    // deinit {
-    //     // WinAppDriver responds waits for a key to return
-    //     try? toStdinPipe.fileHandleForWriting.write(contentsOf: "\n".data(using: .utf8)!)
-    //     process.terminate()
-    // }
+    deinit {
+        // WinAppDriver responds waits for a key to return
+        if let runningProcess = runningProcess {
+            try? runningProcess.toStdinPipe.fileHandleForWriting.write(contentsOf: "\n".data(using: .utf8)!)
+            runningProcess.process.terminate()
+        }
+    }
 
     @discardableResult
     public func send<Request: WebDriverRequest>(_ request: Request) throws -> Request.Response {
---
+++ Sources/WindowsUtilities.swift
@@ -0,0 +1,61 @@
+import Foundation
+import WinSDK
+
+// Helper to determine if a process with a given name is running
+func isProcessRunning(withName processName: String) -> Bool {
+    var processIds: [DWORD] = []
+    var bytesReturned: DWORD = 0
+
+    repeat {
+        processIds = Array(repeating: 0, count: processIds.count + 1024)
+        if !K32EnumProcesses(&processIds, DWORD(processIds.count * MemoryLayout<DWORD>.size), &bytesReturned) {
+            return false
+        }
+    } while bytesReturned == DWORD(processIds.count * MemoryLayout<DWORD>.size)
+
+    let processCount = Int(bytesReturned) / MemoryLayout<DWORD>.size
+
+    for i in 0..<processCount {
+        let processId = processIds[i]
+        guard let processHandle = OpenProcess(DWORD(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ), false, processId) else {
+            continue
+        }
+
+        defer {
+            CloseHandle(processHandle)
+        }
+
+        var processNameBuffer: [WCHAR] = Array(repeating: 0, count: Int(MAX_PATH))
+        if K32GetModuleBaseNameW(processHandle, nil, &processNameBuffer, DWORD(processNameBuffer.count)) > 0 {
+            let processNameString = String(decodingCString: processNameBuffer, as: UTF16.self)
+
+            if processNameString.lowercased() == processName.lowercased() {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+// Helper to enumearate the top level windows and find the one belongging to a given process
+func findTopLevelWindow(for process: Process) -> HWND? {
+    struct Context {
+        let dwProcessId: DWORD
+        var hwnd: HWND?
+    }
+    var context = Context(dwProcessId: DWORD(process.processIdentifier)) 
+    let callback: @convention(c) (HWND?, LPARAM) -> WindowsBool = { (hwnd, lParam) in
+        let pContext = UnsafeMutablePointer<Context>(bitPattern: UInt(lParam))!
+        var pid: DWORD = 0
+        GetWindowThreadProcessId(hwnd, &pid)
+        if pid == pContext.pointee.dwProcessId {
+            pContext.pointee.hwnd = hwnd
+            return false
+        }
+        return true
+    }
+    _ = withUnsafePointer(to: &context) {
+        EnumWindows(callback, LPARAM(UInt(bitPattern: $0)))
+    }
+    return context.hwnd
+}
