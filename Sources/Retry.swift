deb7d741a72280a189d1bcc8ebf1e89d57a147dd
Jeffrey Davis <jeff@thebrowser.company>
2023-07-31 13:01:27 -0700

    Simplify non-returning retry logic.

--- Sources/Retry.swift
+++ Sources/Retry.swift
@@ -20,18 +20,5 @@ internal func retryUntil<T>(_ timeout: TimeInterval, work: () throws -> T?) reth
 
 // Retry the given work using exponential backoff until we use allocated time or work returns true.
 internal func retryUntil(_ timeout: TimeInterval, work: () throws -> Bool) rethrows {
-    var timeUsed: TimeInterval = 0.0
-    var nextTimeout: TimeInterval = 0.001 // Start at 1ms and double until we exhaust time.
-
-    repeat {
-        if try work() { return }
-
-        if timeUsed < timeout {
-            Thread.sleep(forTimeInterval: nextTimeout)
-            timeUsed += nextTimeout
-            nextTimeout = min(nextTimeout * 2.0, timeout - timeUsed)
-        } else {
-            return
-        }
-    } while true
+    _ = try retryUntil(timeout) { try work() ? Optional(true) : nil }
 }
