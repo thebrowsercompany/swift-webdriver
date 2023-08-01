9c16c16ca860bcdf08ac711ee11457ab7d533cab
Jeffrey Davis <jeff@thebrowser.company>
2023-08-01 11:20:43 -0700

    Code review and cleanup.

--- Sources/WinAppDriver.swift
+++ Sources/WinAppDriver.swift
@@ -18,10 +18,8 @@ public class WinAppDriver: WebDriver {
 
         // Ensure WinAppDriver is running.
         if !isProcessRunning(withName: "WinAppDriver.exe") {
-            print("Starting WinAppDriver...")
-
             let path = "\(ProcessInfo.processInfo.environment["ProgramFiles(x86)"]!)\\Windows Application Driver\\WinAppDriver.exe"
-            let commandLine = ["\"" + path + "\"", Self.ip, String(Self.port)].joined(separator: " ")
+            let commandLine = ["\"\(path)\"", Self.ip, String(Self.port)].joined(separator: " ")
             try commandLine.withCString(encodedAs: UTF16.self) { commandLine throws in
                 var startupInfo = STARTUPINFOW()
                 startupInfo.cb = DWORD(MemoryLayout<STARTUPINFOW>.size)
@@ -33,21 +31,17 @@ public class WinAppDriver: WebDriver {
                     nil,
                     nil,
                     false,
-                    DWORD(CREATE_UNICODE_ENVIRONMENT),
+                    DWORD(CREATE_NEW_CONSOLE),
                     nil,
                     nil,
                     &startupInfo,
                     &processInfo
                 ) else {
-                    let error = GetLastError()
-                    printAndFlush("CreateProcessW failed: \(error)")
-                    throw WinAppDriverError.win32Error(lastError: Int(error))
+                    throw WinAppDriverError.win32Error(lastError: Int(GetLastError()))
                 }
 
                 wadProcessInfo = processInfo
             }
-        } else {
-            print("WinAppDriver is already running.")
         }
     }
 
@@ -55,10 +49,9 @@ public class WinAppDriver: WebDriver {
         if let wadProcessInfo {
             CloseHandle(wadProcessInfo.hThread)
 
-            printAndFlush("Terminating WinAppDriver...")
             if !TerminateProcess(wadProcessInfo.hProcess, 0) {
                 let error = GetLastError()
-                printAndFlush("TerminateProcess failed: \(error)")
+                assertionFailure("TerminateProcess failed with error \(error).")
             }
             CloseHandle(wadProcessInfo.hProcess)
         }
