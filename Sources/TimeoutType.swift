496091f2b0b2c020355934c4aeb133cb62421626
Tristan Labelle <git@tristanlabelle.com>
2023-09-03 08:54:22 -0400

    Implement and test session timeouts request

---
+++ Sources/TimeoutType.swift
@@ -0,0 +1,5 @@
+public enum TimeoutType {
+    public static let script = "script"
+    public static let implicitWait = "implicit"
+    public static let pageLoad = "page load"
+}
---
+++ Tests/WebDriverTests/TimeoutTests.swift
@@ -0,0 +1,55 @@
+@testable import WebDriver
+import XCTest
+
+class TimeoutTests: XCTestCase {
+    private static var _winAppDriver: Result<WinAppDriver, any Error>!
+    public var winAppDriver: WinAppDriver! { try? Self._winAppDriver.get() }
+
+    public override class func setUp() {
+        _winAppDriver = Result { try WinAppDriver() }
+    }
+
+    public override class func tearDown() {
+        _winAppDriver = nil
+    }
+
+    public override func setUpWithError() throws {
+        try XCTSkipIf(winAppDriver == nil)
+    }
+
+    func startApp() throws -> Session {
+        // Use a simple app in which we can expect queries to execute quickly
+        let windowsDir = ProcessInfo.processInfo.environment["SystemRoot"]!
+        let capabilities = WinAppDriver.Capabilities(app: "\(windowsDir)\\System32\\winver.exe")
+        return try Session(webDriver: winAppDriver, desiredCapabilities: capabilities)
+    }
+
+    static func time(_ callback: () throws -> Void) rethrows -> Double {
+        let before = DispatchTime.now()
+        try callback()
+        let after = DispatchTime.now()
+        return Double(after.uptimeNanoseconds - before.uptimeNanoseconds) / 1_000_000_000
+    }
+
+    public func testLibraryImplicitWait() throws {
+        let session = try startApp()
+
+        // Test library timeout implementation
+        session.defaultRetryTimeout = 1
+        XCTAssert(try Self.time({ _ = try session.findElement(byAccessibilityId: "IdThatDoesNotExist") }) > 0.5)
+
+        session.defaultRetryTimeout = 0
+        XCTAssert(try Self.time({ _ = try session.findElement(byAccessibilityId: "IdThatDoesNotExist") }) < 0.5)
+    }
+
+    public func testWebDriverImplicitWait() throws {
+        let session = try startApp()
+        session.defaultRetryTimeout = 0
+
+        try session.setTimeout(type: TimeoutType.implicitWait, duration: 1)
+        XCTAssert(try Self.time({ _ = try session.findElement(byAccessibilityId: "IdThatDoesNotExist") }) > 0.5)
+
+        try session.setTimeout(type: TimeoutType.implicitWait, duration: 0)
+        XCTAssert(try Self.time({ _ = try session.findElement(byAccessibilityId: "IdThatDoesNotExist") }) < 0.5)
+    }
+}
