ea275b9bfa1985cf1d46b780b5a19771d150f9de
Tristan Labelle <git@tristanlabelle.com>
2023-09-12 20:10:31 -0400

    Use SHGetKnownFolderPath instead of env vars

--- Sources/WinAppDriver/WindowsSystemPaths.swift
+++ Sources/WinAppDriver/WindowsSystemPaths.swift
@@ -1,17 +1,26 @@
-import class Foundation.ProcessInfo
+import WinSDK
 
 enum WindowsSystemPaths {
-    static var systemDrive: String { ProcessInfo.processInfo.environment["SystemDrive"] ?? "C:" }
+    // Prefer a fallback to bubbling errors since consuming code
+    // use these values as constants to build larger paths
+    // and will handle failures at the time of accessing the file system.
 
-    static var programFilesX86: String {
-        ProcessInfo.processInfo.environment["ProgramFiles(x86)"] ?? "\(systemDrive)\\Program Files (x86)"
+    static let programFilesX86: String = getKnownFolderPath(FOLDERID_ProgramFilesX86, fallback: "C:\\Program Files (x86)")
+    static let windowsDir: String = getKnownFolderPath(FOLDERID_Windows, fallback: "C:\\Windows")
+    static let system32: String = getKnownFolderPath(FOLDERID_System, fallback: "\(windowsDir)\\System32")
+
+    private static func getKnownFolderPath(_ folderId: KNOWNFOLDERID, fallback: String) -> String {
+        (try? getKnownFolderPath(folderId)) ?? fallback
     }
 
-    static var windowsDir: String {
-        ProcessInfo.processInfo.environment["windir"]
-            ?? ProcessInfo.processInfo.environment["SystemRoot"]
-            ?? "\(systemDrive)\\Windows"
+    private static func getKnownFolderPath(_ folderId: KNOWNFOLDERID) throws -> String {
+        var mutableId = folderId
+        var pszPath: PWSTR?
+        let result = WinSDK.SHGetKnownFolderPath(&mutableId, 0, nil, &pszPath)
+        defer { WinSDK.CoTaskMemFree(pszPath) } // no-op on null
+        guard let pszPath, result == S_OK else {
+           throw Win32Error.getLastError(apiName: "SHGetKnownFolderPath") 
+        }
+        return String(decodingCString: pszPath, as: UTF16.self)
     }
-
-    static var system32: String { "\(windowsDir)\\System32" }
 }
