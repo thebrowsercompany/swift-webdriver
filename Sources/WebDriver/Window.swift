599c6bf09675ac1a8d83485290f6ddc3e4ad36e9
Squidonomics <eigthmonthstothemoon@gmail.com>
2024-02-08 21:48:08 -0500

    feat: Init window struct

---
+++ Sources/WebDriver/Window.swift
@@ -0,0 +1,47 @@
+public struct Window {
+
+    var webDriver: WebDriver { session.webDriver }
+    public let session: Session
+    public let handle: String
+
+    public init(session: Session, handle: String) {
+        self.session = session
+        self.handle = handle
+    }
+
+    public var position: (x: Double, y: Double) {
+        get throws {
+            let responseValue = try webDriver.send(Requests.WindowPosition.Get(
+                session: session.id, windowHandle: handle)).value
+            return (responseValue.x, responseValue.y)
+        }
+    }
+
+    public var size: (width: Double, height: Double) {
+        get throws {
+            let responseValue = try webDriver.send(Requests.SessionWindowSize.Get(
+                session: session.id, windowHandle: handle)).value
+            return (responseValue.width, responseValue.height)
+        }
+    }
+
+    /// - Parameter windowHandle: Name of current window
+    /// - Returns: Current window position in form of {x,y} where x and y are the upper left corner of the screen
+    public func position(windowHandle: String) throws -> (x: Int, y: Int) {
+        let response = try webDriver.send(Requests.SessionPosition.Get(session: id, windowHandle: windowHandle))
+        return (x: response.value.x, y: response.value.y)
+    }
+
+    /// - Parameters:
+    ///   - windowHandle: Name of current window
+    ///   - x: Position in the top left corner of the x coordinate
+    ///   - y: Position in the top left corner of the y coordinate
+    public func setPosition(windowHandle: String, x: Double, y: Double) throws {
+        try webDriver.send(Requests.SessionPosition.Post(session: id, windowHandle: windowHandle, x: x, y: y))
+    }
+
+    /// Maximize specific window if :windowHandle is "current" the current window will be maximized
+    public func maximize(windowHandle: String) throws {
+        try webDriver.send(Requests.SessionMaximize(session: id, windowHandle: windowHandle))
+    }
+}
