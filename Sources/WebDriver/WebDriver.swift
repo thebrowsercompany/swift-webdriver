f478ddf0c30713bd3ea320e6d9be37f181b03597
Tristan Labelle <github@tristanlabelle.com>
2024-05-06 12:17:52 -0400

    Fix typo
    Co-authored-by: Jeff <jeffdav@users.noreply.github.com>

--- Sources/WebDriver/WebDriver.swift
+++ Sources/WebDriver/WebDriver.swift
@@ -2,7 +2,7 @@ public protocol WebDriver {
     @discardableResult
     func send<Req: Request>(_ request: Req) throws -> Req.Response
 
-    /// Determines within a given error is inconclusive and should be retried.
+    /// Determines if a given error is inconclusive and should be retried.
     func isInconclusiveInteraction(error: ErrorResponse.Status) -> Bool
 }
 
